fe4afaac97cc074ef1736ed9f275d3d6f2d2a7bd
Nurali Davlatov <nur3.dav.97@gmail.com>
2023-07-27 17:30:51 +0500

    all done

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
 
     $('.btn-menu').on('click', function () {
-        $(this).toggleClass('active');
-        $('header, body').toggleClass('actives');
+        $('.btn-menu, body, .header-right').toggleClass('active');
     })
-       
+
 
     /*---------------------------------------------------end*/
 
@@ -12,8 +11,32 @@ $(document).ready(function () {
     //     e.preventDefault();
     //     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 40, }, 300,)
     // })
-    
-    
-    
+
+
+    /*---------------------------------------------------end*/
+
+    $('.bay-slider').slick({
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+        swipeToSlide: true,
+        slidesToShow: 1,
+        touchThreshold: 9,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    centerPadding: '20px',
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+
+
 });
 
